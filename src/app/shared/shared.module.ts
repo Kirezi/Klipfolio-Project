17a10c16733ad1d12fcca866fe58823de841d93d
Kirezi <pnzey005@uottawa.ca>
2019-12-27 21:32:30 -0500

    added boostrap library to the application

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
-const components = [];
-const modules = [CommonModule, MatButtonModule];
+import { HeaderComponent } from './header/header.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+const components = [HeaderComponent];
+const modules = [
+    CommonModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatFormFieldModule,
+    MatInputModule
+];
 @NgModule({
     declarations: [...components],
     imports: [...modules],
